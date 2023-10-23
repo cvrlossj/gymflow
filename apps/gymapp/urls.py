c33cdfb0fb3dc1ef2396a070324b4663c547681b
Carlos Silva <carlossilvasilvadiaz@gmail.com>
2023-10-23 00:43:29 -0300

    Se agrega el crud para maquinas

--- apps/gymapp/urls.py
+++ apps/gymapp/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.cargarInicio),
-    path('lista',views.cargarListaMaquinas)
+    path('lista',views.cargarListaMaquinas),
+    path('agregarmaquina',views.cargarAgregarMaquina),
+    path('agregarMaquinaForm',views.agregarMaquina),
+    path('editarMaquina/<id>',views.cargarEditarMaquina),
+    path('editarMaquinaForm',views.editarMaquina),
+    path('eliminarMaquina/<id>', views.eliminarMaquina),
 ]
--- apps/gymapp/views.py
+++ apps/gymapp/views.py
@@ -11,3 +11,48 @@ def cargarInicio(request):
 def cargarListaMaquinas(request):
     maquinas = Maquina.objects.all()
     return render(request, "listaMaquinas.html", {"maq": maquinas})
+
+
+def agregarMaquina(request):
+    m_nombre = request.POST['txtMaquina']
+    m_zona_muscular = request.POST['txtZona']
+    m_descripcion = request.POST['txtDesc']
+    m_enlace = request.POST['txtEnlace']
+
+    Maquina.objects.create(nombre=m_nombre, zona_muscular=m_zona_muscular,
+                           descripcion=m_descripcion, enlace_tutorial=m_enlace)
+
+    return redirect('/lista')
+
+
+def cargarAgregarMaquina(request):
+    return render(request, "agregarMaquina.html")
+
+
+def cargarEditarMaquina(request, id):
+    maquinas = Maquina.objects.get(id=id)
+    return render(request, "editarMaquina.html", {"maq": maquinas})
+
+
+def editarMaquina(request):
+    m_id = request.POST['txtId']
+    maquinaBD = Maquina.objects.get(id=m_id)
+    m_nombre = request.POST['txtMaquina']
+    m_zona_muscular = request.POST['txtZona']
+    m_descripcion = request.POST['txtDesc']
+    m_enlace = request.POST['txtEnlace']
+
+    maquinaBD.nombre = m_nombre
+    maquinaBD.zona_muscular = m_zona_muscular
+    maquinaBD.descripcion = m_descripcion
+    maquinaBD.enlace = m_enlace
+
+    maquinaBD.save()
+
+    return redirect('/lista')
+
+
+def eliminarMaquina(request,id):
+    maquina = Maquina.objects.get(id=id)
+    maquina.delete()
+    return redirect('/lista')
