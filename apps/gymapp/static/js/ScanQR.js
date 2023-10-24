d15ae035020d0c9b3f37a819cc73f8bf4f9082aa
Carlos Silva <carlossilvasilvadiaz@gmail.com>
2023-10-24 16:49:18 -0300

    Update Scanner
    Se mejora el escanear un código qr

--- apps/gymapp/static/js/ScanQR.js
+++ apps/gymapp/static/js/ScanQR.js
@@ -1,19 +1,31 @@
-document.getElementById('scanButton').addEventListener('click', function() {
+
+let cameraActive = false;
+
+  document.getElementById('scanButton').addEventListener('click', function() {
     const videoElement = document.createElement('video');
     document.body.appendChild(videoElement);
 
     const scanner = new Instascan.Scanner({ video: videoElement });
 
     scanner.addListener('scan', function(content) {
+      if (!cameraActive) {
+        return; // Si la cámara no está activa, no hagas nada
+      }
+
       // Cuando se escanea un código QR, extraer la URL
       const url = content;
 
-      // Abrir la URL en una nueva pestaña o ventana del navegador
+
+      cameraActive = false;
+      videoElement.srcObject.getTracks().forEach(track => track.stop());
+
+   
       window.open(url, '_blank');
     });
 
     Instascan.Camera.getCameras().then(function(cameras) {
       if (cameras.length > 0) {
+        cameraActive = true; 
         scanner.start(cameras[0]);
       } else {
         alert('No se encontraron cámaras disponibles.');
